757001ab1790b35e2db234cb92f6539c85ec9da6
Cezary Draus <108001877+Taaxuus@users.noreply.github.com>
2023-01-16 21:08:23 +0100

    work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 
+const Person = () => {
+	return (
+		<>
+			<h1>Name : John</h1>
+			<h2>Last Name : Doe</h2>
+			<h2>Age : 30</h2>
+		</>
+	);
+};
+
 const App = () => {
 	return (
 		<div className='App'>
-			<h1>Hello, {2 + 2}</h1>
+			<Person />
 		</div>
 	);
 };
